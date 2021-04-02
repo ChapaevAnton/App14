a8904c2318dabcd7ea4ce06e6352624635aeab88
ChapaevAnton <virsus2009@gmail.com>
2021-04-02 09:44:30 +0700

    commit 14.3

--- src/lambda/Worker.java
+++ src/lambda/Worker.java
@@ -1,8 +1,44 @@
 package lambda;
 
+import java.util.function.Supplier;
+
 @FunctionalInterface
 interface Worker {
 
+    int CONST = 1;
+    String STR = "str";
+
     double rework(String value);
 
+    default int getValue() {
+        return 10;
+    }
+
+    default int setValue(int value) {
+        return value + getValue();
+    }
+
+    private int getInt() {
+        return 100;
+    }
+
+    static double getDouble() {
+        return 50.;
+    }
+
+    Supplier<Integer> supplied = () -> 299;
+
+    class Nested implements Worker {
+
+        interface nestedInterface {
+            Nested getNested();
+        }
+
+        nestedInterface nestedInterface = Nested::new;
+
+        @Override
+        public double rework(String value) {
+            return 0;
+        }
+    }
 }
