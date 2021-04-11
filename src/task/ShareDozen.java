3cbe79f9dbe5456b16109e153bc07c3e84fd2850
Chapaev Anton <virsus2009@gmail.com>
2021-04-11 13:42:21 +0700

    commit 14 total

---
+++ src/task/ShareDozen.java
@@ -0,0 +1,10 @@
+package task;
+
+@FunctionalInterface
+public interface ShareDozen {
+    boolean isSharedDozen(int value);
+
+    static boolean isDozen(int value) {
+        return value % 13 == 0;
+    }
+}
